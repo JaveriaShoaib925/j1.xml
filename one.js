231d403e9a47f6957b6f6a2fedada245444d8bde
Javeria <javeriashoaib01@gmail.com>
2024-01-10 18:18:03 +0500

    comparison products

---
+++ one.js
@@ -0,0 +1,18 @@
+var organization =[
+
+    {
+        "id" : 0,
+        "name" : "Afnan",
+        "salary" : 100000,
+        "designation" : "Manager",
+        "phone" : 12345,
+    },
+
+    {
+        "id" : 25,
+        "name" : "Afnan",
+        "salary" : 100000,
+        "designation" : "Manager"
+        
+    },
+]
